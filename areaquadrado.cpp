ac22f7bb559ad38401f66387aace6afccaf97c02
Ivan Felix <113400369+Ivanfelix93@users.noreply.github.com>
2022-11-03 13:18:41 -0300

    Add files via upload

---
+++ areaquadrado.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	int lado, area;
+	printf(" Digite um vaor para o lado do quadrado: ");
+	scanf("%d", &lado);
+	area=lado*lado;
+	
+	printf(" O valor da area do quadrado e = ");
+	printf("%d", area);
+	
+return 0;		
+}
